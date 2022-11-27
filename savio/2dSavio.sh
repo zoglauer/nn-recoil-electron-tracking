cb04f756bac212e6e5b60c08344764526dcd5d2f
Pmk2021 <pranavmk2003@gmail.com>
2022-11-27 17:00:44 -0500

    Update 2dSavio.sh

--- savio/2dSavio.sh
+++ savio/2dSavio.sh
@@ -39,7 +39,7 @@ module load ml/tensorflow/2.5.0-py37 python/3.7
 echo "Starting execution..."
 
 # --> ADAPT THE FILENAME
-python3 -u RecoilElectrons.py -f /global/home/groups/fc_cosi/Data/RecoilElectronTracking/RecoilElectrons.10k.data
+python3 -u RecoilElectrons.py -f /global/home/groups/fc_cosi/Data/RecoilElectronTracking/RecoilElectrons.100k.data
 
 echo "Waiting for all processes to end..."
 wait
