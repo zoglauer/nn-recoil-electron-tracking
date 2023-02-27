2b776e303fb972308e4c969dfae833f54c62c8c4
Rohan Bohra <rbohra@berkeley.edu>
2023-02-26 21:03:09 -0800

    asdf

--- savio/pytorch_submit.sh
+++ savio/pytorch_submit.sh
@@ -40,8 +40,8 @@ echo "Starting execution..."
 # --> ADAPT THE FILENAME
 cd interaction_network
 
-pip install torch
-pip install torch_geometric
+conda install torch
+conda install torch_geometric
 python3 train.py
 
 echo "Waiting for all processes to end..."
