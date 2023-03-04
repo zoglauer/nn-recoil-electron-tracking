8419ac126ced03ff61dfd3620459849c8b07594f
Rohan Bohra <rbohra@berkeley.edu>
2023-03-04 11:52:24 -0800

    asdf

--- savio/pytorch_submit.sh
+++ savio/pytorch_submit.sh
@@ -37,6 +37,7 @@ module load ml/torch/torch7 python/3.7
 
 # --> ADAPT THE FILENAME
 #source activate torch_env_2
+pip install torch_sparse
 #pip uninstall --user torch
 #pip uninstall --user pyg_lib torch_scatter torch_sparse 
 #pip uninstall --user torch_geometric
