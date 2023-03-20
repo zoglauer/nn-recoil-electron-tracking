0363a2176e2a51e7d4beed50ff69ea3c5a1cd84e
Rohan Bohra <rbohra@berkeley.edu>
2023-03-20 01:23:37 -0700

    asdf

--- savio/pytorch_submit.sh
+++ savio/pytorch_submit.sh
@@ -33,8 +33,9 @@ echo "Starting analysis on host ${HOSTNAME} with job ID ${SLURM_JOB_ID}..."
 
 echo "Loading modules..."
 module purge
-module load python/3.7 ml/torch/torch7
+module load python/3.7 #ml/torch/torch7
 source activate torch_env_3
+conda install torch
 
 echo "Starting execution..."
 python3 interaction_network/train.py
